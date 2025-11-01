dd6de3145864fadc8053a47c76d03a9f105eeffd
saiteki-kai <giuseppe.magazu@gmail.com>
2025-11-01 15:31:31 +0100

    chore: debug key

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from './types.ts';
 
+console.log(process.env.SECRET_TEST_KEY);
 console.log(import.meta.env.SECRET_TEST_KEY);
 
 const supabaseUrl = import.meta.env.SECRET_SUPABASE_URL;
