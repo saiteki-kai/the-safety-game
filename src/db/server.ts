c8b3c100f6128b7203cc6500b2c5b3a7a52f86b1
saiteki-kai <giuseppe.magazu@gmail.com>
2025-11-02 23:09:07 +0100

    test

--- src/db/server.ts
+++ src/db/server.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types.ts";
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.SECRET_SUPABASE_KEY;
+const supabaseKey = process.env.SECRET_SUPABASE_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
 	auth: {
