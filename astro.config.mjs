b3db4a1cff1cc487c7e7163bd8c1e4e35729e8b4
saiteki-kai <giuseppe.magazu@gmail.com>
2025-11-02 13:58:59 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	output: "hybrid",
+	output: "static",
 	adapter: vercel({}),
 
 	vite: {
