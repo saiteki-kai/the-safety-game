a3c666b740cfb4a1c5c2e8814c7afe73df4d61d8
saiteki-kai <giuseppe.magazu@gmail.com>
2025-11-02 13:56:21 +0100

    fix: update depecrated import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import react from "@astrojs/react";
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	output: "hybrid",
-	adapter: vercelServerless(),
+	adapter: vercel({}),
 
 	vite: {
 		plugins: [tailwindcss()],
