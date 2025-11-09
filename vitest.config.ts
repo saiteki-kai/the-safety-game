82435e92c7c0218f2a8e60ef6bfda4d3311f4072
saiteki-kai <giuseppe.magazu@gmail.com>
2025-11-10 00:25:34 +0100

    refactor: remove React plugin from Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,10 @@
 /// <reference types="vitest/config" />
 /// <reference types="vitest/browser" />
 
-import react from "@vitejs/plugin-react";
 import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
   test: {
-    plugins: [react()],
     globals: true,
     environment: "jsdom",
     coverage: {
