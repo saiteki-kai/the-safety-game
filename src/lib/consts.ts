d38c072d1d025c11b02b7123d2ce68c3cd3f150a
saiteki-kai <giuseppe.magazu@gmail.com>
2025-12-02 21:13:52 +0100

    feat: add Copyright component and integrate it into Footer; update email link in privacy page

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,11 +1,12 @@
 export const MAX_TEAM_SIZE = 4;
 export const MAX_DAILY_PROMPTS = 25;
-export const CHALLENGE_END_DATE = new Date("2025-12-17T23:59:59Z");
+export const CHALLENGE_END_DATE = new Date("2026-01-05T23:59:59Z");
+export const LAST_EDIT_DATE_PRIVACY = new Date("2025-12-02T00:00:00Z");
 
 export const FOOTER_LINKS = {
   github: "https://github.com/MIND-Lab",
   hug: "https://huggingface.co/MIND-Lab",
-  email: "mailto:lab.mind.unimib@gmail.com",
+  email: "lab.mind.unimib@gmail.com",
   privacy: "/privacy",
   home: "/",
   dashboard: "/dashboard",
