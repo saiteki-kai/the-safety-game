ccac5280a25dbadd510a9c16689ae4575bc9c2b9
saiteki-kai <giuseppe.magazu@gmail.com>
2025-12-17 01:10:58 +0100

    fix: Update START_SUBMISSIONS_DATE and correct GPT_AVG_SCORE for accurate scoring

--- src/content/consts.ts
+++ src/content/consts.ts
@@ -6,4 +6,4 @@ export const LAST_EDIT_DATE_PRIVACY = new Date("2025-12-02");
 export const START_SUBMISSIONS_DATE = new Date("2025-12-17T16:30:00.000+01:00");
 export const STOP_SUBMISSIONS_DATE = new Date("2026-01-12T23:59:59.999+01:00");
 
-export const GPT_AVG_SCORE = 50; // Average score of ChatGPT submissions
+export const GPT_AVG_SCORE = 0.50; // Average score of ChatGPT submissions
